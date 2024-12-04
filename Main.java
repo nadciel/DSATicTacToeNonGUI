708eed7b42e3cf7c1547160716f1979e1636f942
nadciel <grace.b1645@gmail.com>
2024-12-04 10:46:00 +0700

    TTTNonGUI

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main class of the Java program.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Tic Tac Toe");
+        TTTConsoleNonOO game = new TTTConsoleNonOO();
+        game.play();
+    }
+}
